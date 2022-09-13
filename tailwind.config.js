4cb88cb960c9e5eadd2d4129191784177658b296
rayeesi <hubsoftindia@gmail.com>
2022-09-14 01:46:57 +0530

    grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,8 @@ module.exports = {
         'cust-logo-green':'#019934',
         'cust-pri-clr': '#184bb2',
         'cust-sec-clr': '#019934',
+        'cust-darkest-grey': '#222529',
+        'cust-darker-grey': '#32373b',
         'cust-dark-grey': '#4c4f54',
         'cust-light-grey': '#a1a4b3',
         'cust-lighter-grey': '#e9e9f7',
