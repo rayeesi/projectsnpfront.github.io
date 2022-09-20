c7bf6f922470983bb32a71bd38339fe0d8e87381
rayeesi <hubsoftindia@gmail.com>
2022-09-20 11:36:52 +0530

    jsupdate clickaway

--- entryv1.0.js
+++ entryv1.0.js
@@ -2,33 +2,59 @@
 
 // Navbar toggle start
 // Navbar js
-// inspired by kewinpowells tutorial
+// inspired by kewinpowells & webdevsimplified tutorial
 const primaryNav = document.querySelector('.primary-nav');
 const hamburgerToggler = document.querySelector('.hamburger-button');
 
-hamburgerToggler.addEventListener('click',() => {
-    const visibility = primaryNav.getAttribute('data-visible');
-    if (visibility === 'false'){
-        primaryNav.setAttribute('data-visible','true');
-        hamburgerToggler.setAttribute('aria-expanded','true');
-    }else if(visibility === 'true'){
-        primaryNav.setAttribute('data-visible','false');
-        hamburgerToggler.setAttribute('aria-expanded','false');
-    }
+// hamburgerToggler.addEventListener('click',() => {
+//     const visibility = primaryNav.getAttribute('data-visible');
+//     if (visibility === 'false'){
+//         primaryNav.setAttribute('data-visible','true');
+//         hamburgerToggler.setAttribute('aria-expanded','true');
+//     }else if(visibility === 'true'){
+//         primaryNav.setAttribute('data-visible','false');
+//         hamburgerToggler.setAttribute('aria-expanded','false');
+//     }
 
-    // console.log(visibility);
-})
+//     // console.log(visibility);
+// })
+
+//Navbar Toggler with clickoutside
+    document.addEventListener('click', event => {
+        const isClickInside = primaryNav.contains(event.target)
+    
+        if (!isClickInside) {
+        // The click was OUTSIDE the specifiedElement, do something
+            const visibility = primaryNav.getAttribute('data-visible');
+
+            if(event.target.matches(".hamburger-button")){
+                //console.log("Hamberger");
+                    if (visibility === 'false'){
+                        primaryNav.setAttribute('data-visible','true');
+                        hamburgerToggler.setAttribute('aria-expanded','true');
+                    }else if(visibility === 'true'){
+                        primaryNav.setAttribute('data-visible','false');
+                        hamburgerToggler.setAttribute('aria-expanded','false');
+                    }
+            }else{        
+                
+                //console.log(visibility);
+                    if(visibility === 'true'){
+                        primaryNav.setAttribute('data-visible','false');
+                        hamburgerToggler.setAttribute('aria-expanded','false');
+                    }
+            }
+        }
+    })    
+//Navbar Toggler with clickoutside ends
 
 // Navbar toggle ends
 
+// ############################################################### //
 // search bar toggle
 const searchBar = document.querySelector('.search-bar');
 const toggleSearchBar = document.querySelectorAll('.toggle-search-bar');
-
-
-
-
-
+ 
 toggleSearchBar.forEach(function (item) {
     item.addEventListener('click',() => {
         const searchbarvisibile = searchBar.getAttribute('data-expanded');
