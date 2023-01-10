f89af4488d9ee0a2854f7544f2baf1ac169c3099
rayeesi <hubsoftindia@gmail.com>
2023-01-10 23:27:40 +0530

    webdevnavbar.html

---
+++ webdev/entryv2.0.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// ################## //
+// Navbar toggle start
+// Navbar js
+// inspired by kewinpowells & webdevsimplified tutorial
+const primaryNav = document.querySelector(".primary-nav");
+const hamburgerToggler = document.querySelector(".hamburger-button");
+
+// hamburgerToggler.addEventListener('click',() => {
+//     const visibility = primaryNav.getAttribute('data-visible');
+//     if (visibility === 'false'){
+//         primaryNav.setAttribute('data-visible','true');
+//         hamburgerToggler.setAttribute('aria-expanded','true');
+//     }else if(visibility === 'true'){
+//         primaryNav.setAttribute('data-visible','false');
+//         hamburgerToggler.setAttribute('aria-expanded','false');
+//     }
+
+//     // console.log(visibility);
+// })
+
+//Navbar Toggler with clickoutside
+document.addEventListener("click", (event) => {
+  const isClickInside = primaryNav.contains(event.target);
+
+  if (!isClickInside) {
+    // The click was OUTSIDE the specifiedElement, do something
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (event.target.matches(".hamburger-button")) {
+      //console.log("Hamberger");
+      if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", "true");
+        hamburgerToggler.setAttribute("aria-expanded", "true");
+      } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        hamburgerToggler.setAttribute("aria-expanded", "false");
+      }
+    } else {
+      //console.log(visibility);
+      if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        hamburgerToggler.setAttribute("aria-expanded", "false");
+      }
+    }
+  }
+});
+//Navbar Toggler with clickoutside ends
+
+// Navbar toggle ends
+
+// ################## //
+//  Dropdown Menu //
+// inspired by webdevsimplified tutorial
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches(".dropdown-toggle");
+
+  if (!isDropdownButton && e.target.closest(".dropdown") != null) return;
+  console.log(isDropdownButton);
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    console.log("isdrop");
+    currentDropdown = e.target.closest(".dropdown");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll(".dropdown.active").forEach((dropDown) => {
+    if (dropDown === currentDropdown) return;
+    dropDown.classList.remove("active");
+  });
+});
